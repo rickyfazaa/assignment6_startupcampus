cd500d0f666c719ac5344e4eb4d27c104d9357a5
rickyfazaa <the_eiza_rhitza@yahoo.com>
2023-05-15 07:21:05 +0700

    add multiplication & division operator, add error handle

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from math_function import add
+from math_function import * #asteris sama dengan import all modul dalam sebuah file (modul).
 
 
 def main():
 
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
+    while True:
+        try:
+            data_1 = int(input("masukkan input 1 :"))
+            data_2 = int(input("masukkan input 2 :"))
+            if type(data_1) == int and type(data_2) == int:
+                break
+        except ValueError:
+              print("Please Input an Integer.")        
+    
+    while True:
+        operator = input("masukkan operator :")
+        if operator == "+":
+            result = addition(data_1, data_2)
+            break
+        elif operator == "*":
+            result = multiplication(data_1, data_2)
+            break
+        elif operator == "/":
+            result = division(data_1, data_2)
+            break
+        else:
+            print("Operator belum ditambahkan, coba input ulang operator matematika seperti pertambahan(+), perkalian(*), dan pembagian(/)")
 
-    if operator == "+":
-        result = add(data_1, data_2)
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
